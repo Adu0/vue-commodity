9413457affee1fec15a296c7fd261c3e3b952395
cole <591212363@qq.com>
2018-11-23 15:17:02 +0800

    完成了发表评论

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,8 @@ import MemberComponent from '../pages/member/'
 import ShopcarComponent from '../pages/shopcar/'
 import SearchComponent from '../pages/search/'
 import newList from '../pages/newList/'
-import newInfo from '../pages/newinfo'
+import newInfo from '../pages/newinfo/'
+import photoList from '../pages/photoList/'
 
 Vue.use(Router)
 
@@ -18,7 +19,8 @@ export default new Router({
     { path: "/shopcar", component: ShopcarComponent },
     { path: "/search", component: SearchComponent },
     { path: "/home/newList", component: newList },
-    { path: "/home/newsinfo/:id", component: newInfo }
+    { path: "/home/newsinfo/:id", component: newInfo },
+    {path:'/home/photoList',component:photoList}
   ],
   linkActiveClass: "mui-bar-tab mui-tab-item mui-active"
 });
